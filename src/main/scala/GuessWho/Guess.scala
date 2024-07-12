1d5d30ab6fd365c03f22a9f15b4db0ddbf22d441
Tom Burt-Gray <tom.burt.gray@Toms-Laptop.local>
2024-07-12 11:03:29 +0100

    unimplemented guess logic

--- src/main/scala/GuessWho/Guess.scala
+++ src/main/scala/GuessWho/Guess.scala
@@ -1,11 +1,34 @@
 package GuessWho
 
-
-
-
+/** name: String,
+ hasHair: Boolean,
+ hasFacialHair: Boolean,
+ hasGlasses: Boolean,
+ hasHat: Boolean,
+ gender:Gender,
+ eyeColour: EyeColour,
+ hairColour: HairColour */
 
 class Guess (character:Character){
 
-  def
+  def guessHasHair(guess:String): Boolean = {
+    ???
+  }
+  def guessHasFacialHair(guess:String):Boolean = {
+    ???
+  }
+  def guessHasGlasses(guess:String): Boolean = {
+    ???
+  }
+  def guessHasHat(guess:String): Boolean = {
+    ???
+  }
+
+
+  def guessGender(guess:String): Boolean = { ??? }
+
+  def guessEyeColour(guess:String): Boolean = { ??? }
+
+  def guessHairColour(guess:String): Boolean = { ??? }
 
 }
