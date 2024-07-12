c52ab6f7e088896d6c5a322eb5ff250826e8ea38
Jamie Letts <jamieletts2210@gmail.com>
2024-07-12 10:38:10 +0100

    Build character class and traits

---
+++ src/main/scala/character.scala
@@ -0,0 +1,33 @@
+
+
+sealed trait EyeColour
+case object BLUE extends EyeColour
+case object GREEN extends EyeColour
+case object BROWN extends EyeColour
+
+sealed trait HairColour
+case object BLONDE extends HairColour
+case object BRUNETTE extends HairColour
+case object RED extends HairColour
+
+
+sealed trait Gender
+case object MALE extends Gender
+case object FEMALE extends Gender
+
+
+case class Character(
+                      name: String,
+                      hasHair: Boolean,
+                      hasFacialHair: Boolean,
+                      hasGlasses: Boolean,
+                      hasHat: Boolean,
+                      gender:Gender,
+                      eyeColour: EyeColour,
+                      hairColour: HairColour
+                    )
+
+val Tom: Character = Character(name = "Tom", hasHair = true, hasFacialHair = false, hasGlasses = true, hasHat = false, gender = MALE, eyeColour = GREEN , hairColour = BRUNETTE)
+val Jamie: Character = Character(name = "Jamie", hasHair = false, hasFacialHair = true, hasGlasses = true, hasHat = true, gender = MALE, eyeColour = BLUE, hairColour = BLONDE)
+
+
